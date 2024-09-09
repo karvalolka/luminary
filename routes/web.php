fcf7508af4294e1a2084dda5b4352357dc4cda39
ChiLo <pyha1994@yandex.ru>
2024-09-09 19:58:04 +0300

    оптимизация шаблона; + стили для шаблона; + админка; отредактировал админку;

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\Admin\Main\AdminIndexController;
 use App\Http\Controllers\Main\IndexController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [IndexController::class, '__invoke']);
+
+Route::prefix('admin')->group(function (){
+    Route::get('/', [AdminIndexController::class, '__invoke']);
+});
 
 Auth::routes();
